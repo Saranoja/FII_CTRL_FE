ea69e3c1e806c37e5fe21847283a0281bd44cae1
Saranoja <Saranoja@users.noreply.github.com>
2021-05-18 14:31:40 +0300

    feat: added notifications styling

--- ctrl/src/components/notificationsDrawer/NotificationsDrawer.js
+++ ctrl/src/components/notificationsDrawer/NotificationsDrawer.js
@@ -1,43 +1,50 @@
 import React from 'react';
+import * as R from 'ramda';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import * as R from 'ramda';
-import Drawer from '@material-ui/core/Drawer';
-import Box from '@material-ui/core/Box';
+import Alert from '@material-ui/lab/Alert';
 import Button from '@material-ui/core/Button';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
+import Typography from '@material-ui/core/Typography';
 import StyledNotificationsDrawer from './NotificationsDrawer.style';
 
 const action = (
-  <Button color="secondary" size="small">
+  <Button color="primary" size="small" disableElevation>
     Dismiss
   </Button>
 );
 
 const NotificationsDrawer = ({ isOpen, onClose, notificationsList }) => {
   return (
-    <StyledNotificationsDrawer>
-      <Drawer anchor="right" open={isOpen} onClose={onClose}>
-        <Box minWidth="200px">
-          {notificationsList.length ? (
-            R.map(
-              (notification) => (
-                <div
-                  key={`${notification.title}${notification.text}${notification.author}}`}
-                >
-                  <SnackbarContent
-                    message={`${notification.author} posted a new announcement in ${notification.group}`}
-                    action={action}
-                  />
-                </div>
-              ),
-              notificationsList
-            )
-          ) : (
-            <p> No new notifications </p> // TODO: style this
-          )}
-        </Box>
-      </Drawer>
+    <StyledNotificationsDrawer
+      anchor="right"
+      open={isOpen}
+      onClose={onClose}
+      classes={{
+        paperAnchorRight: 'drawer-paper',
+      }}
+    >
+      {notificationsList.length ? (
+        R.map(
+          (notification) => (
+            <div
+              key={`${notification.title}${notification.text}${notification.author}}`}
+            >
+              <Alert
+                severity="info"
+                action={action}
+                className="snackbar-notification-content"
+              >
+                {`${notification.author} posted a new announcement in ${notification.group}`}
+              </Alert>
+            </div>
+          ),
+          notificationsList
+        )
+      ) : (
+        <Typography variant="h6" className="empty-drawer-text">
+          No new notifications
+        </Typography>
+      )}
     </StyledNotificationsDrawer>
   );
 };
--- ctrl/src/components/notificationsDrawer/NotificationsDrawer.style.js
+++ ctrl/src/components/notificationsDrawer/NotificationsDrawer.style.js
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
+import Drawer from '@material-ui/core/Drawer';
 
-const StyledNotificationsDrawer = styled.div`
+const StyledNotificationsDrawer = styled(Drawer)`
   ${({ theme }) => `
-    `}
+    .drawer-paper {
+      min-width: ${theme.spacing(35)}px;
+    }
+
+    .snackbar-notification-content, .empty-drawer-text {
+      margin: ${theme.spacing(0.5)}px ${theme.spacing(1)}px;
+    }
+  `}
 `;
 
 export default StyledNotificationsDrawer;
