cf446235bdba6cf877cbea73e869766585fce50b
Saranoja <Saranoja@users.noreply.github.com>
2021-06-22 18:20:02 +0300

    fix: updated gitignore and hocs

--- ctrl/src/hocs/index.js
+++ ctrl/src/hocs/index.js
@@ -1,5 +1,4 @@
 export { default as withInView } from './withInView';
-export { default as withLogin } from './withLogin';
 export { default as withAlreadyLoggedIn } from './withAlreadyLoggedIn';
 export { default as withSignedIn } from './withSignedIn';
 export { default as withTeacher } from './withTeacher';
